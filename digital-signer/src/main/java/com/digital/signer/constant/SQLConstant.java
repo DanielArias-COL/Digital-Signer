8eed1cdedec098589d8800248fe10a995adcdee5
PAGOSGDE\jvargas <jvargas@powwi.co>
2024-08-17 08:02:58 -0500

    Add a returning for create user

--- digital-signer/src/main/java/com/digital/signer/constant/SQLConstant.java
+++ digital-signer/src/main/java/com/digital/signer/constant/SQLConstant.java
@@ -2,5 +2,5 @@ package com.digital.signer.constant;
 
 public class SQLConstant {
 
-    public static final String SAVE_USER = "INSERT INTO public.user_ds (username, \"password\") VALUES(?, ?)";
+    public static final String SAVE_USER = "INSERT INTO public.user_ds (username, \"password\") VALUES(?, ?) RETURNING id";
 }
