61c65a2a532ec91c4d27bc583dd09e26e8e78275
Daniel Andres Arias <126762117+escanor904@users.noreply.github.com>
2024-08-16 21:21:59 -0500

    initial configuratión

---
+++ digital-signer/src/main/java/com/digital/signer/DigitalSignerApplication.java
@@ -0,0 +1,13 @@
+package com.digital.signer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignerApplication.class, args);
+	}
+
+}
---
+++ digital-signer/src/test/java/com/digital/signer/DigitalSignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.signer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalSignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
